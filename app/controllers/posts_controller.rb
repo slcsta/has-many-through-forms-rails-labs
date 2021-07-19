9df31f375567dd785233892e29f77fcf049dbc39
Sarah <sarahlchavez@gmail.com>
2021-07-19 11:39:36 -0700

    commit tree 5c7af7e576e5be6c9f6f48f07c38009f92cd1c6d

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @categories = @post.categories
   end
 
   def index
@@ -9,9 +10,11 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
   end
 
   def create
+    # byebug
     post = Post.create(post_params)
     redirect_to post
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: ->(attributes){ attributes['name'].blank? }
+
+  # def unique_users
+  #   self.users.uniq
+  # end
 
 end
